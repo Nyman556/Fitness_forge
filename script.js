1fb2b7a5fb76bb24f3114131afd88fefe35719e1
Johan Nyman <johan.nyman97@gmail.com>
2023-04-18 13:11:16 +0200

    added hero

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-feather.replace();
+updateIcons();
+
+const hamburger = document.getElementById("hamburger-menu");
+const dropdown = document.getElementById("dropdown");
+let state = 0;
+
+function toggle() {
+	if (state == 0) {
+		hamburger.innerHTML = '<i data-feather="x"></i>';
+		state++;
+		hamburger.setAttribute("currentState", state);
+		updateIcons();
+		dropdown.classList.remove("hidden");
+		dropdown.classList.add("flex");
+		return;
+	} else hamburger.innerHTML = '<i data-feather="menu"></i>';
+	hamburger.setAttribute("currentState", "0");
+	state--;
+	updateIcons();
+	dropdown.classList.remove("flex");
+	dropdown.classList.add("hidden");
+}
+
+function updateIcons() {
+	feather.replace();
+}
