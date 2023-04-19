335e99b8ba641bdc774de221fc174f43b52e7cd8
Johan Nyman <johan.nyman97@gmail.com>
2023-04-19 17:45:16 +0200

    added location carousel

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-updateIcons();
-
 const hamburger = document.getElementById("hamburger-menu");
 const dropdown = document.getElementById("dropdown");
+const slidesLoaded = document.getElementsByClassName("slide");
+
+let slideIndex = 1;
 let state = 0;
+
+updateIcons();
+showSlides();
 
 function toggle() {
 	if (state == 0) {
@@ -21,4 +25,41 @@ function toggle() {
 
 function updateIcons() {
 	feather.replace();
+}
+
+var header = document.getElementById("header");
+window.onscroll = function () {
+	stickyHeader();
+};
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+	if (window.pageYOffset > sticky) {
+		header.classList.add("sticky");
+	} else {
+		header.classList.remove("sticky");
+	}
+}
+
+function plusSlides(n) {
+	showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+	showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+	let i;
+	let slides = slidesLoaded;
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "block";
 }
