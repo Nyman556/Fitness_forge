c329965813ec24d8fe666e3ef3b6bb806b1f9fd8
Johan Nyman <johan.nyman97@gmail.com>
2023-04-20 17:01:21 +0200

    table content

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const hamburger = document.getElementById("hamburger-menu");
 const dropdown = document.getElementById("dropdown");
-const slidesLoaded = document.getElementsByClassName("slide");
 
 let slideIndex = 1;
 let state = 0;
@@ -51,7 +50,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
 	let i;
-	let slides = slidesLoaded;
+	let slides = document.getElementsByClassName("slide");
 	if (n > slides.length) {
 		slideIndex = 1;
 	}
@@ -63,3 +62,103 @@ function showSlides(n) {
 	}
 	slides[slideIndex - 1].style.display = "block";
 }
+
+function togglePlan(n) {
+	let buttons = document.getElementsByClassName("plans-toggle");
+	for (i = 0; i < buttons.length; i++) {
+		buttons[i].classList.remove("plans-active");
+	}
+	buttons[n].classList.add("plans-active");
+	renderTable(n);
+}
+
+const tableData = {
+	pushData: {
+		exercises: [
+			"Flat Bench",
+			"Incline Bench",
+			"Shoulder Press",
+			"Lateral Raises",
+			"Pushdowns",
+			"Overhead Extensions",
+		],
+		sets: [5, 3, 5, 3, 3, 3],
+		reps: [5, 10, 5, 12, 12, 12],
+		time: ["60s", "60s", "60s", "60s", "35s", "60s"],
+	},
+	pullData: {
+		exercises: [
+			"Pull ups",
+			"Pulldowns",
+			"Barbell row",
+			"Seated cable row",
+			"Shrugs",
+			"Bicep curls",
+		],
+		sets: [4, 4, 3, 3, 4, 4],
+		reps: ["AMRAP", 10, 8, 10, 10, 10],
+		time: ["60s", "60s", "120s", "60s", "60s", "60s"],
+	},
+	legsData: {
+		exercises: [
+			"Squats",
+			"Rear deadlift",
+			"lunges",
+			"Hip thrusts",
+			"Extensions",
+			"Hamstring curls",
+		],
+		sets: [5, 4, 4, 3, 3, 3],
+		reps: [5, 8, 12, 8, 12, 12],
+		time: ["60s", "60s", "60s", "60s", "35s", "60s"],
+	},
+};
+
+function renderTable(n) {
+	const targetedTableData = Object.values(tableData)[n];
+	tBody = document.getElementById("table").firstElementChild;
+	tBody.innerHTML = `					
+			<tr id="table-head">
+				<th>Exercise Name</th>
+				<th>Sets</th>
+				<th>Reps</th>
+				<th>Time/set</th>
+				<th>Complete</th>
+			</tr>`;
+	for (i = 0; i < 6; i++) {
+		tData = document.createElement("td");
+		tRow = document.createElement("tr");
+		tData.innerHTML = `${targetedTableData.exercises[i]}`;
+		tRow.append(tData);
+		tRow.classList.add(`row-${i}`);
+		tBody.append(tRow);
+		if (i < 6) {
+			tData = document.createElement("td");
+			tData.innerHTML = `${targetedTableData.sets[i]}`;
+			tRow.append(tData);
+			tBody.append(tRow);
+		}
+		if (i < 6) {
+			tData = document.createElement("td");
+			tData.innerHTML = `${targetedTableData.reps[i]}`;
+			tRow.append(tData);
+			tBody.append(tRow);
+		}
+		if (i < 6) {
+			tData = document.createElement("td");
+			tData.innerHTML = `${targetedTableData.time[i]}`;
+			tRow.append(tData);
+			tBody.append(tRow);
+		}
+		if (i < 6) {
+			tData = document.createElement("td");
+			tCheckbox = document.createElement("input");
+			tCheckbox.setAttribute("type", "checkbox");
+			tData.append(tCheckbox);
+			tRow.append(tData);
+			tBody.append(tRow);
+		}
+	}
+}
+
+renderTable(1);
